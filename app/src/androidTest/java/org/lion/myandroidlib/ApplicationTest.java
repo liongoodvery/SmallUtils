0f3f58c00480b0620a503342d329a1c24cff58cc
lion_good <lion_good@163.com>
2016-09-07 15:03:09 +0800

    remove app

--- app/src/androidTest/java/org/lion/myandroidlib/ApplicationTest.java
+++ app/src/androidTest/java/org/lion/myandroidlib/ApplicationTest.java
@@ -2,6 +2,7 @@ package org.lion.myandroidlib;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.LruCache;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,8 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void test13() throws Exception {
     }
 }
